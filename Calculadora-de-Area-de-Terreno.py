97fda7fc82395d3d819061124badd4294592f711
Israel-moreira <israel.moreira.ilsm@gmail.com>
2024-06-25 23:07:05 -0300

    Create Calculadora-de-Area-de-Terreno.py
    Introdução e prática do 'def'

---
+++ Calculadora-de-Area-de-Terreno.py
@@ -0,0 +1,16 @@
+# Projeto: Calculadora de Área de Terreno
+
+def area(largura, comprimento):
+    print('-' * 39)
+    print(f'A área do seu terreno é: {largura * comprimento:.2f} M²')
+    print('-' * 39)
+
+def intro():
+        print('~' * 39)
+        print(f'{"CALCULADORA DE TERRENO - v1":^39}')
+        print('~' * 39)
+
+intro()
+largura = float(input('Digite a largura do seu terreno: '))
+comprimento = float(input('Digite o comprimento do seu terreno: '))
+area(largura, comprimento)
