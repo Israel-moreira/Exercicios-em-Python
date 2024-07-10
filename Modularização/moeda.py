a68d97f9c70c2e9dc53795a94b39f4099c42f181
Israel-moreira <israel.moreira.ilsm@gmail.com>
2024-07-09 23:19:49 -0300

    Estudo de modularização

---
+++ Modularização/moeda.py
@@ -0,0 +1,17 @@
+def dobro(n):
+    resultado = n * 2
+    return resultado
+
+def metade(n):
+    resultado = n / 2
+    return resultado
+
+def aumentar(n, p):
+    perc = n * (p * 0.01)
+    resultado = n + perc
+    return resultado
+
+def diminuir(n, p):
+    perc = n * (p * 0.01)
+    resultado = n - perc
+    return resultado
