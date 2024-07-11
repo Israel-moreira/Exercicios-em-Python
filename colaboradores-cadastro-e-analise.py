ec72c435c43dc21efad2d6ea48587441ba745ad3
Israel-moreira <israel.moreira.ilsm@gmail.com>
2024-07-10 23:27:10 -0300

    Update colaboradores-cadastro-e-analise.py

--- colaboradores-cadastro-e-analise.py
+++ colaboradores-cadastro-e-analise.py
@@ -10,25 +10,25 @@ while True:
     pessoa.clear()
     pessoa['nome'] = str(input('Nome: '))
     while True:
-        pessoa['sexo'] = str(input('Sexo: [M/F] '))
-        if pessoa['sexo'] in 'MmFf':
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
             break
         print('ERRO! Por favor, digite apenas M ou F.')
     pessoa['idade'] = int(input('Idade: '))
     soma += pessoa['idade']
     colaboradores.append(pessoa.copy())
     while True:
-        resp = str(input('Quer continuar? [S/N] '))
-        if resp in 'SsNn':
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
             break
         print('ERRO! Responda apenas S ou N ')
-    if resp in 'Nn':
+    if resp in 'N':
         break
 lin()
-print(f'Ao todo temos {len(colaboradores)} colaboradores cadastradas.')
+print(f'Ao todo temos {len(colaboradores)} colaboradores cadastrados.')
 media = soma / len(colaboradores)
 lin()
-print(f'A média de idades dos colaboradores cadastradas é: {media:5.2f} anos')
+print(f'A média de idade dos colaboradores cadastrados é: {media:5.2f} anos')
 print(f'As mulheres cadastradas são: ', end='')
 for p in colaboradores:
     if p['sexo'] in 'Ff':
