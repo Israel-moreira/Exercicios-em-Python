c65e32516828d826954ae661be14a7f5cd2eec83
Israel-moreira <israel.moreira.ilsm@gmail.com>
2024-07-16 22:54:26 -0300

    Update main.py

--- Modularização/main.py
+++ Modularização/main.py
@@ -1,7 +1,7 @@
 import moeda
 
-valor = float(input('Digite um valor: R$'))
-print(f'O dobro de R${valor:.2f} é R${moeda.dobro(valor):.2f}')
-print(f'A metade de R${valor:.2f} é R${moeda.metade(valor):.2f}')
-print(f'Aumentando 10%, temos R${moeda.aumentar(valor, 10):.2f}')
-print(f'Reduzindo 13%, temos R${moeda.diminuir(valor, 13):.2f}')
+p = float(input('Digite um valor: R$'))
+print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}')
+print(f'O dobro de {moeda.moeda(p)} é {moeda.moeda(moeda.dobro(p))}')
+print(f'Aumentando 10%, temos {moeda.moeda(moeda.aumentar(p, 10))}')
+print(f'Reduzindo 13%, temos {moeda.moeda(moeda.diminuir(p, 13))}')
