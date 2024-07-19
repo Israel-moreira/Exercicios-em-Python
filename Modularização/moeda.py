0ea40faea23533dbfbcde45da03cd29bdc6ee814
Israel-moreira <israel.moreira.ilsm@gmail.com>
2024-07-18 22:33:52 -0300

    Update moeda.py

--- Modularização/moeda.py
+++ Modularização/moeda.py
@@ -1,18 +1,18 @@
-def dobro(preço=0):
+def dobro(preço=0, formato=False):
     res = preço * 2
-    return res
+    return res if not formato else moeda(res)
 
-def metade(preço=0):
+def metade(preço=0, formato=False):
     res = preço / 2
-    return res
+    return res if not formato else moeda(res)
 
-def aumentar(preço=0, taxa=0):
+def aumentar(preço=0, taxa=0, formato=False):
     res = preço + (preço * taxa/100)
-    return res
+    return res if not formato else moeda(res)
 
-def diminuir(preço=0, taxa=0):
+def diminuir(preço=0, taxa=0, formato=False):
     res = preço - (preço * taxa/100)
-    return res
+    return res if not formato else moeda(res)
 
 def moeda(preço=0, moeda='R$'):
     return f'{moeda}{preço:.2f}'.replace('.', ',')
