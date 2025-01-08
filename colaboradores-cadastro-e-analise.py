fb40021a0bd2c8fcd60c55edc550b7ab787555c5
Israel-moreira <159722525+Israel-moreira@users.noreply.github.com>
2025-01-07 22:59:12 -0300

    Update colaboradores-cadastro-e-analise.py

--- colaboradores-cadastro-e-analise.py
+++ colaboradores-cadastro-e-analise.py
@@ -5,39 +5,49 @@ media = soma = 0
 def lin():
     print('-=' * 30)
 
-
 while True:
     pessoa.clear()
-    pessoa['nome'] = str(input('Nome: '))
+    pessoa['nome'] = input('Nome: ').strip()
+    
     while True:
-        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        pessoa['sexo'] = input('Sexo: [M/F] ').strip().upper()[0]
         if pessoa['sexo'] in 'MF':
             break
         print('ERRO! Por favor, digite apenas M ou F.')
+    
     pessoa['idade'] = int(input('Idade: '))
     soma += pessoa['idade']
     colaboradores.append(pessoa.copy())
+    
     while True:
-        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        resp = input('Quer continuar? [S/N] ').strip().upper()[0]
         if resp in 'SN':
             break
-        print('ERRO! Responda apenas S ou N ')
-    if resp in 'N':
+        print('ERRO! Responda apenas S ou N.')
+    
+    if resp == 'N':
         break
+
 lin()
 print(f'Ao todo temos {len(colaboradores)} colaboradores cadastrados.')
 media = soma / len(colaboradores)
 lin()
 print(f'A média de idade dos colaboradores cadastrados é: {media:5.2f} anos')
-print(f'As mulheres cadastradas são: ', end='')
+
+# Mulheres cadastradas
+print('As mulheres cadastradas são: ', end='')
 for p in colaboradores:
-    if p['sexo'] in 'Ff':
-        print(f'({p['nome']}) ', end='')
+    if p['sexo'] == 'F':
+        print(f'({p["nome"]}) ', end='')
 print()
+
 lin()
+
+# Colaboradores acima da média de idade
 print('Lista de colaboradores que estão acima da média de idade: ', end='')
 for p in colaboradores:
     if p['idade'] >= media:
-        print(f'({p['nome']}) ', end='')
+        print(f'({p["nome"]}) ', end='')
 print()
+
 print('<<< Encerrado >>>')
